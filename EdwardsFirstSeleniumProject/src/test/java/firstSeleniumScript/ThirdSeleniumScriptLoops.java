72400076e030ac6e5f61a72279d46a48012ba5e6
Edward Zaplotynskyi <edward.downtown@gmail.com>
2023-11-17 17:09:17 -0500

    static dropdown

--- EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/ThirdSeleniumScriptLoops.java
+++ EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/ThirdSeleniumScriptLoops.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,19 +14,32 @@ public class ThirdSeleniumScriptLoops {
 	
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://amazon.com/sign-in");
-		
-		WebElement emailOrPhone = driver.findElement(By.xpath("//div[@class='a-box']/div/div/input"));
-		WebElement continueButton = driver.findElement(By.xpath("//input[@type='submit']"));
-//		WebElement loginButton = driver.findElement(By.cssSelector("button[id*='u_0_']"));
+		driver.get("https://amazon.com/");
+
+		driver.navigate().refresh();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		emailOrPhone.sendKeys("+19803713112");
-		continueButton.click();
+		driver.findElement(By.cssSelector("[id='searchDropdownBox']")).click();
+		driver.findElement(By.cssSelector("[value$=stripbooks]")).click();
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("tiktok");
+		driver.findElement(By.cssSelector("input[id='nav-search-submit-button']")).click();
+
 		
-		driver.navigate().back();
 		
-		driver.findElement(By.xpath("//div[@class='a-box']/div/div/input")).clear();
+//		driver.manage().window().setPosition(new Point(908, 24));
+//		driver.findElement(By.id("searchDropdownBox")).click();
+//		WebElement emailOrPhone = driver.findElement(By.xpath("//div[@class='a-box']/div/div/input"));
+//		WebElement continueButton = driver.findElement(By.xpath("//input[@type='submit']"));
+//		WebElement loginButton = driver.findElement(By.cssSelector("button[id*='u_0_']"));
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		driver.manage().timeouts().ex
+		
+//		emailOrPhone.sendKeys("+19803713112");
+//		continueButton.click();
+//		
+//		driver.navigate().back();
+//		
+//		driver.findElement(By.xpath("//div[@class='a-box']/div/div/input")).clear();
 		
 	}
 	
