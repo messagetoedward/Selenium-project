250ce82eecf6d4db22b8aed3e796c50d0a5bbdca
Edward Zaplotynskyi <edward.downtown@gmail.com>
2023-12-16 15:09:50 -0500

    for + if + else + break in dropdown list

--- EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/ForLoopsConditionalStatements.java
+++ EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/ForLoopsConditionalStatements.java
@@ -1,6 +1,7 @@
 package firstSeleniumScript;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByXPath;
@@ -19,55 +20,113 @@ public class ForLoopsConditionalStatements {
 		Thread.sleep(4000);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		
-		driver.findElement(By.xpath("//*[@id=\"b2indexPage\"]/div[22]/div/div/div/div[1]/div[1]/div/button")).click();
+//		driver.findElement(By.xpath("//*[@id=\"b2indexPage\"]/div[22]/div/div/div/div[1]/div[1]/div/button")).click();
 		
 		WebElement passenger = driver.findElement(By.xpath("//*[@id='indexsearch']/div[2]/div/form/div[1]/div[3]/div/button"));
-		passenger.click();
+//		passenger.click();
 		
-		WebElement addAdultPassenger = driver.findElement(By.xpath("//*[@id=\":rf:\"]/div/div[1]/div[2]/button[2]"));
-		WebElement exreactAdultPassenger = driver.findElement(By.xpath("//*[@id=\":rf:\"]/div/div[1]/div[2]/button[1]"));
-		WebElement doneButton = driver.findElement(By.xpath("//*[@id=\":rf:\"]/button"));
+		WebElement typeDestination = driver.findElement(By.id(":re:"));
+		typeDestination.click();
+		typeDestination.sendKeys("ch");
 		
-		addAdultPassenger.click();
+		Thread.sleep(Duration.ofSeconds(2));
 		
-		int i =3;
-		while (i<10) {
-			addAdultPassenger.click();
-			i++;
+		List<WebElement> autoComplete = driver.findElements(By.xpath("//*[@id='autocomplete-results']/div/ul/li"));
+		
+		System.out.println(autoComplete.size());
+		
+		Thread.sleep(Duration.ofSeconds(2));
+		
+		for (WebElement each : autoComplete) {
+			String eachName=each.getText();
+			System.out.println(eachName);
+			if(eachName.contains("Charlotte") /*("Charlotte\n"
+					+ "North Carolina, United States")*/) {
+				each.click();
+				System.out.println("clicked");
+				break;
+			} else {
+				System.out.println("was not clicked - panic >=O");
+			}
+		} 
+		
+		/* count and name each <a> tag START
+		 
+		List<WebElement> generalAtag = driver.findElements(By.tagName("a"));
+		System.out.println(generalAtag.size());
+		
+		for (WebElement link:generalAtag) {
+			String linkName=link.getText();
+			System.out.println(linkName);
 		}
 		
-		int k = 10;
-		while (k>1) {
-			exreactAdultPassenger.click();
-			i--;
-		}
+		count and name each <a> tag END */
 		
-//		for (int i=2; i<10; i++) {
-//			addAdultPassenger.click();
-//		}
-//		
-//		for (int i=10; i>2; i--) {
-//			exreactAdultPassenger.click();
-//		}
 		
-//		int i=2;
-//		while (i<5) {
-//			addAdultPassenger.click();
-//			i++;
+		
+		
+		
+//		WebElement addAdultPassenger = driver.findElement(By.xpath("//*[@id=\":rf:\"]/div/div[1]/div[2]/button[2]"));
+//		WebElement exreactAdultPassenger = driver.findElement(By.xpath("//*[@id=\":rf:\"]/div/div[1]/div[2]/button[1]"));
+//		WebElement doneButton = driver.findElement(By.xpath("//*[@id=\":rf:\"]/button"));
+		
+//		addAdultPassenger.click();
+		
+//		WebElement travelForWorkDiv = driver.findElement(By.xpath("//div[@data-testid='searchbox-footer']/div[1]")); 
+//		WebElement travelForWorkInput = driver.findElement(By.id(":rg:"));
+//			
+		
+//		if(travelForWorkInput.isSelected()==true) {
+//			System.out.println(travelForWorkInput.isSelected());
+//		} else {
+//			travelForWorkDiv.click();
 //		}
+//	
 //		
-//		int k=5;
-//		while (k>3) {
-//			exreactAdultPassenger.click();
-//			k--;
-//		}
+//		System.out.println(travelForWorkInput.isSelected());		
+				
+		
+		
+	//for, while loops, add, extract START
+		//int i =3;
+		//while (i<10) {
+		//addAdultPassenger.click();
+		//i++;
+		//}
+		//		
+		//int k = 10;
+		//while (k>1) {
+		//exreactAdultPassenger.click();
+		//i--;
+		//}
+				
+		//for (int i=2; i<10; i++) {
+		//addAdultPassenger.click(); 
+		//}
+		//		
+		//for (int i=10; i>2; i--) {
+		//exreactAdultPassenger.click();
+		//}
+				
+		//int i=2;
+		//while (i<5) {
+		//addAdultPassenger.click();
+		//i++;
+		//}
+		//		
+		//int k=5;
+		//while (k>3) {
+		//exreactAdultPassenger.click();
+		//k--;
+		//}
 		
 		//for (int i=2; i<5; i++) {
 		//	addAdultPassenger.click();
 		//	System.out.println(driver.findElement(By.xpath("//*[@id=\":rf:\"]/div/div[1]/div[2]/span")).getText());
 		//}
 		
-//		doneButton.click();
+		//doneButton.click();
+	//for, while loops, add, extract END
 		
 	}
 
