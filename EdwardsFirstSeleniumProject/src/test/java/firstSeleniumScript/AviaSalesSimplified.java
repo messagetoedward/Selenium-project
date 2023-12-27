f886720ae5457eb37079468356335e78e72c6cfc
Edward Zaplotynskyi <edward.downtown@gmail.com>
2023-12-27 15:46:14 -0500

    screenshot, and working on assertions

--- EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/AviaSalesSimplified.java
+++ EdwardsFirstSeleniumProject/src/test/java/firstSeleniumScript/AviaSalesSimplified.java
@@ -3,16 +3,23 @@ package firstSeleniumScript;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class AviaSalesSimplified {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub	
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://aviasales.com");
@@ -65,6 +72,10 @@ public class AviaSalesSimplified {
 			
 		}
 		
+		TakesScreenshot screenshot = (TakesScreenshot)driver;
+		File screenCapture = screenshot.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenCapture, new File("/Users/mgid/Documents/GitHub/screensnap.png"));
+		
 		WebElement startDateFieldButton = driver.findElement(By.cssSelector("button[data-test-id='start-date-field']"));
 		startDateFieldButton.click();
 		
@@ -72,11 +83,40 @@ public class AviaSalesSimplified {
 //		List <WebElement> startDateMonthList = driver.findElements(By.xpath("/html/body/div[7]/div[2]/div/div/div/div/form/div[1]/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[2]/div[1]/div[1]/div/select/option"));	
 		startDateMonth.selectByIndex(4);
 		
-		WebElement startDate = driver.findElement(By.cssSelector("div[data-test-id='date-13.01.2024']"));
-		startDate.click();
+		List <WebElement> datesLoop = driver.findElements(By.cssSelector("div[role='gridcell']"));
 		
+		for (WebElement eachDate : datesLoop) {
+			String ariaLabelValue = eachDate.getAttribute("aria-label");
+			if (ariaLabelValue.contains("Wed Jan 03 2024")) {
+				eachDate.click();
+				eachDate.click();
+				System.out.println("date " + ariaLabelValue + " found and clicked");
+			} else {
+				System.out.println(ariaLabelValue + " date not found");
+			}
+			
+			if (ariaLabelValue.contains("Sat Feb 03 2024")) {
+				eachDate.click();
+				System.out.println("date " + ariaLabelValue + " found and clicked");
+				break;
+			} else {
+				System.out.println(ariaLabelValue + " date not found");
+			}
+		}
+		
+			File screenCapture2 = screenshot.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenCapture2, new File("/Users/mgid/Documents/GitHub/screensnap1.png"));
+		
+		System.out.println(driver.getTitle());	
+			
+		//Assert.assertEquals(driver.getTitle(), false, null);
+		
+		/* WebElement startDate = driver.findElement(By.cssSelector("div[data-test-id='date-13.01.2024']"));
+		startDate.click();
+		startDate.click();
+
 		WebElement endDate = driver.findElement(By.cssSelector("div[data-test-id='date-20.01.2024']"));
-		endDate.click();
+		endDate.click(); */
 		
 		
 		
